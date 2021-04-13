4967382d22c7341897f5c344a717d10773ab41aa
gurunitin <nitin.guru@rxdigital.co.in>
2021-04-13 20:41:00 +0530

    changed app.py

--- app.py
+++ app.py
@@ -27,13 +27,8 @@ api.add_resource(ItemList,'/items')
 api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+# if __name__ == '__main__':
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+db.init_app(app)
 
-    app.run(port=5000)
+app.run(port=5000)
