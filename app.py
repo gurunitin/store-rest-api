f1dbd582f38501cf2dd97c5030eaddff22309103
gurunitin <nitin.guru@rxdigital.co.in>
2021-04-14 18:51:23 +0530

    updated appn.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
-from db import db
 
 from security import authenticate,identity
 from resources.user import UserRegister
@@ -27,8 +26,13 @@ api.add_resource(ItemList,'/items')
 api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
 
-db.init_app(app)
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
 
-app.run(port=5000)
+    app.run(port=5000)
