fca0c18359a64c13e4a35dea93846a53940439f0
gurunitin <nitin.guru@rxdigital.co.in>
2021-04-14 19:07:10 +0530

    updated run.py and app.py

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='nitin'
 api = Api(app)
 
-# @app.before_first_request
-# def create_tables():
-#     db.create_all()
 
 # app.config['JWT_AUTH_URL_RULE'] = '/login' #to change the end point from default /auth to /login
 jwt = JWT(app,authenticate,identity) #/auth endpoint
@@ -29,10 +26,4 @@ api.add_resource(UserRegister,'/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+    app.run(port=5000,debug=True)
